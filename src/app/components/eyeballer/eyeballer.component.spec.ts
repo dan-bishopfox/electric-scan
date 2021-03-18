0658c48d11012d026560eec2702bd80677fbdd43
moloch-- <875022+moloch--@users.noreply.github.com>
2021-03-18 16:21:12 -0500

    ng update

--- src/app/components/eyeballer/eyeballer.component.spec.ts
+++ src/app/components/eyeballer/eyeballer.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { EyeballerComponent } from './eyeballer.component';
 
@@ -6,7 +6,7 @@ describe('EyeballerComponent', () => {
   let component: EyeballerComponent;
   let fixture: ComponentFixture<EyeballerComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ EyeballerComponent ]
     })
